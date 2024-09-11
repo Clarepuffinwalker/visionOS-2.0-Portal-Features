56b311f4ca1ad55a60faf6c5ae38455eb7c3355b
Clarepuffinwalker <zhouminhui1993@outlook.com>
2024-09-11 15:30:21 +0800

    visionOS 2.0 Portal Feature Update Trial

--- Portal/ContentView.swift
+++ Portal/ContentView.swift
@@ -2,7 +2,7 @@
 //  ContentView.swift
 //  Portal
 //
-//  Created by 周铁 on 2024/9/10.
+//  Created by ClareZhou on 2024/9/10.
 //
 
 import SwiftUI
@@ -11,43 +11,83 @@ import RealityKitContent
 
 struct ContentView: View {
 
-    @State private var enlarge = false
-
     var body: some View {
         RealityView { content in
-            // Add the initial RealityKit content
-            if let scene = try? await Entity(named: "Scene", in: realityKitContentBundle) {
-                content.add(scene)
-            }
-        } update: { content in
-            // Update the RealityKit content when SwiftUI state changes
-            if let scene = content.entities.first {
-                let uniformScale: Float = enlarge ? 1.4 : 1.0
-                scene.transform.scale = [uniformScale, uniformScale, uniformScale]
+            
+            let world = makeWorld()
+            let portal = makePortal(world:world)
+            
+            content.add(world)
+            content.add(portal)
+            
             }
         }
-        .gesture(TapGesture().targetedToAnyEntity().onEnded { _ in
-            enlarge.toggle()
-        })
-        .toolbar {
-            ToolbarItemGroup(placement: .bottomOrnament) {
-                VStack (spacing: 12) {
-                    Button {
-                        enlarge.toggle()
-                    } label: {
-                        Text(enlarge ? "Reduce RealityView Content" : "Enlarge RealityView Content")
-                    }
-                    .animation(.none, value: 0)
-                    .fontWeight(.semibold)
+    public func makeWorld() -> Entity{
+        let world = Entity()
+        world.components[WorldComponent.self] = .init()
+        
+        //IBL Lighting
+        if let environment = try? EnvironmentResource.load(named: "Light"){
+            world.components[ImageBasedLightComponent.self] = .init(source: .single(environment),intensityExponent: 2.0)
+            world.components[ImageBasedLightReceiverComponent.self] = .init(imageBasedLight: world)
+        }
+        
+        // World Background - Image
+        /*
+        let resource = try! TextureResource.load(named: "pokemon03")
+        var material = UnlitMaterial()
+        material.color = .init(texture: .init(resource))
+        let background = Entity()
+        background.components.set(ModelComponent(
+            mesh: .generateSphere(radius: 0.8),
+                                           materials: [material]))
+        background.scale.x *= -1
+        world.addChild(background)
+         */
+        // World Background - Model
+        if let pokemonCenter = try? Entity.load(named: "Pokemon_Center_Emerald") {
+            pokemonCenter.scale = [0.003,0.003,0.003]
+            pokemonCenter.position = [0,-0.5,-0.5]
+            world.addChild(pokemonCenter)
+        }
+        
+        //3D Objects
+        if let pokemon = try? Entity.load(named: "Mew_Flying") {
+            pokemon.scale = [0.005,0.005,0.005]
+            pokemon.transform.rotation = simd_quatf(angle: .pi/(-6), axis: [1,0,0])
+            pokemon.position = [0,-0.25,0]
+            
+            // Add PortalCrossingComponent
+            pokemon.components[PortalCrossingComponent.self] = .init()
+            
+            world.addChild(pokemon)
+        }
 
-                    ToggleImmersiveSpaceButton()
-                }
-            }
+        return world
+    }
+    
+    public func makePortal(world:Entity) -> Entity{
+        let portal = Entity()
+        portal.components[ModelComponent.self] = .init(mesh: .generatePlane(width:0.8, height: 0.8, cornerRadius:0.8), materials: [PortalMaterial()])
+        portal.components[PortalComponent.self] = .init(target: world)
+        
+        
+        if #available(visionOS 2.0, *) {
+            let portalComponent = PortalComponent(
+                target: world,
+                clippingMode: .disabled,
+                crossingMode: .plane(.positiveZ)
+            )
+            portal.components.set(portalComponent)
+        } else {
+            // Fallback on earlier versions
         }
+        
+        return portal
     }
 }
 
+
 #Preview(windowStyle: .volumetric) {
     ContentView()
-        .environment(AppModel())
 }
--- Portal/PortalApp.swift
+++ Portal/PortalApp.swift
@@ -10,25 +10,12 @@ import SwiftUI
 @main
 struct PortalApp: App {
 
-    @State private var appModel = AppModel()
 
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .environment(appModel)
         }
         .windowStyle(.volumetric)
 
-        ImmersiveSpace(id: appModel.immersiveSpaceID) {
-            ImmersiveView()
-                .environment(appModel)
-                .onAppear {
-                    appModel.immersiveSpaceState = .open
-                }
-                .onDisappear {
-                    appModel.immersiveSpaceState = .closed
-                }
-        }
-        .immersionStyle(selection: .constant(.mixed), in: .mixed)
     }
 }
